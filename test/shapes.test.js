874ee02c7ccd9a7192460cb228a098a21bb4e4c5
Ronin1702 <kchen@me.com>
2023-07-08 17:22:00 -0400

    change square y value from 50 to -50 so that i will appear in the middle of the screen instead of bottom, changed text height in squares

--- test/shapes.test.js
+++ test/shapes.test.js
@@ -28,7 +28,7 @@ describe('Shapes', () => {
     it('should render an SVG string for Square with the given color', () => {
       const shape = new Square();
       shape.setColor("green");
-      expect(shape.render()).toEqual('<rect x="50" y="50" width="200" height="200" fill="green" />');
+      expect(shape.render()).toEqual('<rect x="50" y="-50" width="200" height="200" fill="green" />');
     });
   });
 });
